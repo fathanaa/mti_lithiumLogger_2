ee5fa8383283089c10b9abbaef55a2232c59f323
severe.Proc <60329788+fathanaa@users.noreply.github.com>
2021-07-10 11:48:37 +0700

    update

--- Form1.cs
+++ Form1.cs
@@ -16,7 +16,7 @@ namespace mti_lithiumLogger_2
             InitializeComponent();
 
             serialPort = new SerialPort();
-            serialPort.PortName = "COM4";
+            serialPort.PortName = "COM3";
             serialPort.BaudRate = 9600;
             serialPort.DtrEnable = true;
             serialPort.Open();
@@ -40,13 +40,54 @@ namespace mti_lithiumLogger_2
 
             Thread thread_v01 = new Thread(new ThreadStart(() => v01(dataFromArduino, dard)));
             thread_v01.Start();
+
+            Thread thread_v02 = new Thread(new ThreadStart(() => v02(dataFromArduino, dard)));
+            thread_v02.Start();
+
+            Thread thread_v03 = new Thread(new ThreadStart(() => v03(dataFromArduino, dard)));
+            thread_v03.Start();
+
+            Thread thread_v04 = new Thread(new ThreadStart(() => v04(dataFromArduino, dard)));
+            thread_v04.Start();
+
+            Thread thread_v05 = new Thread(new ThreadStart(() => v05(dataFromArduino, dard)));
+            thread_v05.Start();
+
+            Thread thread_v06 = new Thread(new ThreadStart(() => v06(dataFromArduino, dard)));
+            thread_v06.Start();
+
+            Thread thread_v07 = new Thread(new ThreadStart(() => v07(dataFromArduino, dard)));
+            thread_v07.Start();
+
+            Thread thread_v08 = new Thread(new ThreadStart(() => v08(dataFromArduino, dard)));
+            thread_v08.Start();
+
+            Thread thread_v09 = new Thread(new ThreadStart(() => v09(dataFromArduino, dard)));
+            thread_v09.Start();
+
+            Thread thread_v10 = new Thread(new ThreadStart(() => v10(dataFromArduino, dard)));
+            thread_v10.Start();
+
+            Thread thread_v11 = new Thread(new ThreadStart(() => v11(dataFromArduino, dard)));
+            thread_v11.Start();
+
+            Thread thread_v12 = new Thread(new ThreadStart(() => v12(dataFromArduino, dard)));
+            thread_v12.Start();
+
+            Thread thread_v13 = new Thread(new ThreadStart(() => v13(dataFromArduino, dard)));
+            thread_v13.Start();
+
+            Thread thread_v14 = new Thread(new ThreadStart(() => v14(dataFromArduino, dard)));
+            thread_v14.Start();
+
+           
         }
 
         public void arus(string dataFromArduino, int dard)
         {
             if (int.TryParse(dataFromArduino, out dard))
             {
-                if (dard == 0x01014359)
+                if (dard == 0x01164100)
                 {
                     while (true)
                     {
@@ -66,7 +107,7 @@ namespace mti_lithiumLogger_2
         {
             if (int.TryParse(dataFromArduino, out dard))
             {
-                if (dard == 0x0102671)
+                if (dard == 0x01156229)
                 {
                     while (true)
                     {
@@ -86,7 +127,7 @@ namespace mti_lithiumLogger_2
         {
             if (int.TryParse(dataFromArduino, out dard))
             {
-                if (dard == 0x01032703)
+                if (dard == 0x01014359)
                 {
                     while (true)
                     {
@@ -101,6 +142,269 @@ namespace mti_lithiumLogger_2
                 }
             }
         }
+
+        public void v02(string dataFromArduino, int dard)
+        {
+            if (int.TryParse(dataFromArduino, out dard))
+            {
+                if (dard == 0x0102671)
+                {
+                    while (true)
+                    {
+                        dard += 1;
+                        string result = DecimalToHexadecimal(dard);
+                        this.Invoke(new MethodInvoker(delegate ()
+                        {
+                            tb_v02.Text = result;
+                        }));
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+
+        public void v03(string dataFromArduino, int dard)
+        {
+            if (int.TryParse(dataFromArduino, out dard))
+            {
+                if (dard == 0x01032703)
+                {
+                    while (true)
+                    {
+                        dard += 1;
+                        string result = DecimalToHexadecimal(dard);
+                        this.Invoke(new MethodInvoker(delegate ()
+                        {
+                            tb_v03.Text = result;
+                        }));
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+
+        public void v04(string dataFromArduino, int dard)
+        {
+            if (int.TryParse(dataFromArduino, out dard))
+            {
+                if (dard == 0x01042185)
+                {
+                    while (true)
+                    {
+                        dard += 1;
+                        string result = DecimalToHexadecimal(dard);
+                        this.Invoke(new MethodInvoker(delegate ()
+                        {
+                            tb_v04.Text = result;
+                        }));
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+
+        public void v05(string dataFromArduino, int dard)
+        {
+            if (int.TryParse(dataFromArduino, out dard))
+            {
+                if (dard == 0x01053395)
+                {
+                    while (true)
+                    {
+                        dard += 1;
+                        string result = DecimalToHexadecimal(dard);
+                        this.Invoke(new MethodInvoker(delegate ()
+                        {
+                            tb_v05.Text = result;
+                        }));
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+
+        public void v06(string dataFromArduino, int dard)
+        {
+            if (int.TryParse(dataFromArduino, out dard))
+            {
+                if (dard == 0x01062371)
+                {
+                    while (true)
+                    {
+                        dard += 1;
+                        string result = DecimalToHexadecimal(dard);
+                        this.Invoke(new MethodInvoker(delegate ()
+                        {
+                            tb_v06.Text = result;
+                        }));
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+
+        public void v07(string dataFromArduino, int dard)
+        {
+            if (int.TryParse(dataFromArduino, out dard))
+            {
+                if (dard == 0x01074064)
+                {
+                    while (true)
+                    {
+                        dard += 1;
+                        string result = DecimalToHexadecimal(dard);
+                        this.Invoke(new MethodInvoker(delegate ()
+                        {
+                            tb_v07.Text = result;
+                        }));
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+
+        public void v08(string dataFromArduino, int dard)
+        {
+            if (int.TryParse(dataFromArduino, out dard))
+            {
+                if (dard == 0x01083165)
+                {
+                    while (true)
+                    {
+                        dard += 1;
+                        string result = DecimalToHexadecimal(dard);
+                        this.Invoke(new MethodInvoker(delegate ()
+                        {
+                            tb_v08.Text = result;
+                        }));
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+
+        public void v09(string dataFromArduino, int dard)
+        {
+            if (int.TryParse(dataFromArduino, out dard))
+            {
+                if (dard == 0x01093895)
+                {
+                    while (true)
+                    {
+                        dard += 1;
+                        string result = DecimalToHexadecimal(dard);
+                        this.Invoke(new MethodInvoker(delegate ()
+                        {
+                            tb_v09.Text = result;
+                        }));
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+
+        public void v10(string dataFromArduino, int dard)
+        {
+            if (int.TryParse(dataFromArduino, out dard))
+            {
+                if (dard == 0x01102628)
+                {
+                    while (true)
+                    {
+                        dard += 1;
+                        string result = DecimalToHexadecimal(dard);
+                        this.Invoke(new MethodInvoker(delegate ()
+                        {
+                            tb_v10.Text = result;
+                        }));
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+
+        public void v11(string dataFromArduino, int dard)
+        {
+            if (int.TryParse(dataFromArduino, out dard))
+            {
+                if (dard == 0x01112610)
+                {
+                    while (true)
+                    {
+                        dard += 1;
+                        string result = DecimalToHexadecimal(dard);
+                        this.Invoke(new MethodInvoker(delegate ()
+                        {
+                            tb_v11.Text = result;
+                        }));
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+
+        public void v12(string dataFromArduino, int dard)
+        {
+            if (int.TryParse(dataFromArduino, out dard))
+            {
+                if (dard == 0x01122890)
+                {
+                    while (true)
+                    {
+                        dard += 1;
+                        string result = DecimalToHexadecimal(dard);
+                        this.Invoke(new MethodInvoker(delegate ()
+                        {
+                            tb_v12.Text = result;
+                        }));
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+
+        public void v13(string dataFromArduino, int dard)
+        {
+            if (int.TryParse(dataFromArduino, out dard))
+            {
+                if (dard == 0x01131865)
+                {
+                    while (true)
+                    {
+                        dard += 1;
+                        string result = DecimalToHexadecimal(dard);
+                        this.Invoke(new MethodInvoker(delegate ()
+                        {
+                            tb_v13.Text = result;
+                        }));
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+
+        public void v14(string dataFromArduino, int dard)
+        {
+            if (int.TryParse(dataFromArduino, out dard))
+            {
+                if (dard == 0x01144275)
+                {
+                    while (true)
+                    {
+                        dard += 1;
+                        string result = DecimalToHexadecimal(dard);
+                        this.Invoke(new MethodInvoker(delegate ()
+                        {
+                            tb_v14.Text = result;
+                        }));
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+
+    
+
 
         public static string DecimalToHexadecimal(int dec)
         {
--- sketch_may26a/sketch_may26a.ino
+++ sketch_may26a/sketch_may26a.ino
@@ -5,25 +5,24 @@ void setup()
 
 void loop() 
 {
-    int ite = 1; 
     
     unsigned long data1 = 0x01014359; //16859993
     unsigned long data2 = 0x0102671; //16918129
     unsigned long data3 = 0x01032703; //16918275
     unsigned long data4 = 0x01042185; //16982405
     unsigned long data5 = 0x01053395; //17052565
-//    unsigned long data6 = 0x2371; //17113969
-//    unsigned long data7 = 0x4064; //17186916
-//    unsigned long data8 = 0x3165; //17248613
-//    unsigned long data9 = 0x3895; //17315989
-//    unsigned long data10 = 0x2628; //17376808
-//    unsigned long data11 = 0x2610; //17835536
-//    unsigned long data12 = 0x2890; //354734325
-//    unsigned long data13 = 0x1865; //340936696 
-//    unsigned long data14 = 0x4275; //64777696
-//    unsigned long data15 = 0x6229; //870449591
-//    unsigned long data16 = 0x4100; //743958359
-//    
+    unsigned long data6 = 0x01062371; //17113969
+    unsigned long data7 = 0x01074064; //17186916
+    unsigned long data8 = 0x01083165; //17248613
+    unsigned long data9 = 0x01093895; //17315989
+    unsigned long data10 = 0x01102628; //17376808
+    unsigned long data11 = 0x01112610; //17835536
+    unsigned long data12 = 0x01122890; //354734325
+    unsigned long data13 = 0x01131865; //340936696 
+    unsigned long data14 = 0x01144275; //64777696
+    unsigned long temp = 0x01156229; //870449591
+    unsigned long arus = 0x01164100; //743958359
+    
 
 //    float a = (float)data1/16;       // 295707998 / 16 = 18481749.875
 //    float b = a - (int)a;           // 18481749.875 - 18481749 = 0.875
@@ -36,6 +35,18 @@ void loop()
     Serial.println(data3);
     Serial.println(data4);
     Serial.println(data5);
+    Serial.println(data6);
+    Serial.println(data7);
+    Serial.println(data8);
+    Serial.println(data9);
+    Serial.println(data10);
+    Serial.println(data11);
+    Serial.println(data12);
+    Serial.println(data13);
+    Serial.println(data14);
+    Serial.println(temp);
+    Serial.println(arus);
+    
 
   delay(1000);
 }
